c1f018e5eb7a16870e33d14ed18dd8737126ea98
Miles Hoang <mileskhoang@gmail.com>
2024-05-19 11:35:41 -0700

    poacher stuff

--- Assets/ProjectileBehavior.cs
+++ Assets/ProjectileBehavior.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class ProjectileBehavior : MonoBehaviour
 {
     public float speed = 3;
-    public Rigidbody2D rb;
+    public Rigidbody2D rb; 
+    public float damage = 3;
 
     // Update is called once per frame
     private void Update()
@@ -11,9 +12,18 @@ public class ProjectileBehavior : MonoBehaviour
         rb.velocity = transform.right * speed;
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    void OnColliOnsionEnter2D(Collision2D other)
     {
+        if(other.tag == "Enemy") {
+            // Deal damage to the enemy
+            Enemy enemy = other.GetComponent<Enemy>();
+
+            if(enemy != null) {
+                enemy.Health -= damage;
+            }
+        }
         Destroy(gameObject);
     }
+    
 
 }
